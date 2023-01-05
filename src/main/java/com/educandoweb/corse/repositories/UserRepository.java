dce96984d3c57f8c77541de96e40e96d03d03171
eduardomanzini <eduardo_manzini@hotmail.com>
2023-01-05 11:34:42 -0300

    project created, user entity and resource, h2 database e testprofile e jpa, JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/educandoweb/corse/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.educandoweb.corse.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoweb.corse.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	
+	
+}
---
+++ src/test/java/com/educandoweb/corse/CorseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.corse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
