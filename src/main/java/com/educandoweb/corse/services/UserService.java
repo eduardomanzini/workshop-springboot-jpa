1c988555cd2f7f06bdaf5cf8246c3e3918248f37
eduardomanzini <eduardo_manzini@hotmail.com>
2023-01-11 11:12:07 -0300

    User update

--- src/main/java/com/educandoweb/corse/services/UserService.java
+++ src/main/java/com/educandoweb/corse/services/UserService.java
@@ -32,4 +32,15 @@ public class UserService {
 		repository.deleteById(id);
 	}
 	
+	public User udpdate(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setIphone(obj.getIphone());	
+	}
 }
