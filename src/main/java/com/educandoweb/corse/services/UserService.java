f0d067c2e5113bae8d460ccfa0f277c67e0348f5
eduardomanzini <eduardo_manzini@hotmail.com>
2023-01-12 12:01:08 -0300

    Exception handling - delete

--- src/main/java/com/educandoweb/corse/services/UserService.java
+++ src/main/java/com/educandoweb/corse/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.corse.entities.User;
 import com.educandoweb.corse.repositories.UserRepository;
+import com.educandoweb.corse.services.exceptions.DatabaseException;
 import com.educandoweb.corse.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,14 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 	
 	public User udpdate(Long id, User obj) {
